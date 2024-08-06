a733449f15e2dd59550ba873f7e5f7419039c2c6
Stephan Spengler <stephanspengler93@gmail.com>
2024-08-06 12:34:52 +0200

    query actions before executing them and having one single proper error message on failure

--- src/TerrainManager.ts
+++ src/TerrainManager.ts
@@ -10,18 +10,6 @@ import * as Shapes from "./Shapes";
 import type { Fun2Num, LookUp, SelectionDesc } from "./types";
 import { isActive, toolMode, toolProfile, toolShape } from "./Window";
 
-function executeAction(x: number, y: number, height: number, slope: number): void {
-    const args = {
-        x: x << 5,
-        y: y << 5,
-        height: height << 1,
-        style: slope,
-    };
-    context.queryAction("landsetheight", args,
-        result => result.error || context.executeAction("landsetheight", args),
-    );
-}
-
 type Num4 = [number, number, number, number];
 
 function getSurface(x: number, y: number): SurfaceElement | undefined {
@@ -51,9 +39,9 @@ function getSurfaceZ(x: number, y: number): Num4 {
     ) as Num4;
 }
 
-function setSurfaceZ(x: number, y: number, fractional: Num4, up = true): void {
+function getActionArgs(x: number, y: number, fractional: Num4, up = true): LandSetHeightArgs | undefined {
     const surface = getSurface(x, y);
-    if (!surface) return;
+    if (!surface) return undefined;
 
     let integral = fractional.map(corner => Math.round(corner));
 
@@ -85,8 +73,53 @@ function setSurfaceZ(x: number, y: number, fractional: Num4, up = true): void {
         return slope;
     }, 0);
 
-    // set new height and slope
-    executeAction(x, y, height, slope);
+    return {
+        x: x << 5,
+        y: y << 5,
+        height: height << 1,
+        style: slope,
+    };
+}
+
+function executeAll(tiles: CoordsXY[], fun: (tile: CoordsXY) => LandSetHeightArgs | undefined): boolean {
+    if (tiles.length === 0) return true;
+
+    let cost = 0;
+    let errorResult: GameActionResult = {};
+    const queryOk: LandSetHeightArgs[] = [];
+
+    tiles.map(fun).filter(args => args !== undefined).forEach(args => context.queryAction("landsetheight", args, result => {
+        switch (result.error) {
+            case 0:
+            case 4:
+                queryOk.push(args);
+                cost += result.cost || 0;
+                break;
+            default:
+                errorResult = result;
+        }
+    }));
+
+    // all actions fail
+    if (queryOk.length === 0) {
+        ui.showError(
+            errorResult.errorTitle || "",
+            errorResult.errorMessage || "",
+        );
+        return false;
+    }
+
+    // insufficient funds
+    if (cost > park.cash && !park.getFlag("noMoney")) {
+        ui.showError(
+            context.formatString("{STRINGID}", 5637),
+            context.formatString("{STRINGID}", 827, cost),
+        );
+        return false;
+    }
+
+    queryOk.forEach(args => context.executeAction("landsetheight", args));
+    return true;
 }
 
 class Profile<T = Num4> {
@@ -145,12 +178,14 @@ export function setSelection(selectionDesc: SelectionDesc): void {
 
 const cornerOffsets = [[1, 1], [1, 0], [0, 0], [0, 1]];
 export function apply(delta: number): void {
-    tiles.forEach(({ x, y }) => {
+    const changedProfile = currentProfile.lazyClone();
+    if (executeAll(tiles, ({ x, y }) => {
         const oldProfile = originalProfile.getZ(x, y);
         const newProfile = cornerOffsets.map(([dx, dy], idx) => strategy(oldProfile[idx], deltaProfile(x + dx, y + dy) * delta)) as Num4;
-        currentProfile.setZ(x, y, newProfile);
-        setSurfaceZ(x, y, newProfile);
-    });
+        changedProfile.setZ(x, y, newProfile);
+        return getActionArgs(x, y, newProfile);
+    }))
+        currentProfile = changedProfile;
 }
 
 function getStrategy(selectionDesc: SelectionDesc): Fun2Num {
@@ -200,33 +235,43 @@ export function smooth(tiles: CoordsXY[], delta: number): void {
             .map(([x, y, idx]) => originalProfile.getZ(x, y)[idx]);
         return fun1(...cornerHeights);
     });
-    tiles.forEach(({ x, y }) => {
+    executeAll(tiles, ({ x, y }) => {
         const oldProfile = originalProfile.getZ(x, y);
         const newProfile = cornerOffsets.map(([dx, dy], idx) => fun2(oldProfile[idx] + delta, profile.getZ(x + dx, y + dy))) as Num4;
-        setSurfaceZ(x, y, newProfile, delta > 0);
+        return getActionArgs(x, y, newProfile, delta > 0);
     });
 }
 
 export function flat(tiles: CoordsXY[], delta: number): void {
-    tiles.forEach(({ x, y }) => {
+    executeAll(tiles, ({ x, y }) => {
         const surface = getSurface(x, y);
         if (surface) {
-            let height = surface.baseHeight >> 1;
+            let height = surface.baseHeight;
             if (delta > 0) {
                 if (surface.slope)
-                    height++;
+                    height += 2;
                 if (surface.slope & 0x10)
-                    height++;
+                    height += 2;
             }
-            executeAction(x, y, height, 0);
+            return {
+                x: x << 5,
+                y: y << 5,
+                height: height,
+                style: 0,
+            };
         }
     });
 }
 
 export function rough(tiles: CoordsXY[]): void {
-    tiles.forEach(({ x, y }) => {
+    executeAll(tiles, ({ x, y }) => {
         const surface = getSurface(x, y);
         if (surface)
-            executeAction(x, y, surface.baseHeight >> 1, Math.round(Math.random() * 16));
+            return {
+                x: x << 5,
+                y: y << 5,
+                height: surface.baseHeight,
+                style: Math.round(Math.random() * 16),
+            };
     });
 }
